9895b12374f61ed4e7882a1bff1b7f4df047a9eb
JayeetaMondal <72766760+Jayeetam@users.noreply.github.com>
2022-09-16 20:38:27 +0530

    Add files via upload

---
+++ curryingClosure.js
@@ -0,0 +1,19 @@
+let multiply = function(x) {
+
+	return function (y){
+
+		console.log(x*y);
+
+	}
+
+}
+
+let multiplyByTwo = multiply(2);
+
+multiplyByTwo(3);
+
+
+
+let multiplyByThree = multiply(3);
+
+multiplyByThree(10);
