0df2a941e16cc3636d067118cf05955fdfa7c7e5
JayeetaMondal <72766760+Jayeetam@users.noreply.github.com>
2022-09-20 16:39:51 +0530

    StudentEligibleOrNot

---
+++ Studenteligible.js
@@ -0,0 +1,10 @@
+function eligible(marks){
+    var ans="not eigible";
+            if(marks>=40){
+                ans="eligible";
+            }
+
+            return(ans);
+            
+}
+console.log(eligible(39));
